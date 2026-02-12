05a08a9e3a16733b9b2be77c6b3a4b41d3b8b5b1
unknown <diego.f.dias@outlook.com>
2026-02-12 14:22:34 -0300

    Added some secrets to my Github actions repository so I can actually pass these tests in the CI pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
+require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Map environment variables from Github Actions to Cypress env
+      config.env.standardUsername = process.env.STANDARD_USERNAME || config.env.standardUsername
+      config.env.lockedoutUsername = process.env.LOCKEDOUT_USERNAME || config.env.lockedoutUsername
+      config.env.password = process.env.PASSWORD || config.env.password
+      return config
     },
     baseUrl: 'https://www.saucedemo.com'
   },
